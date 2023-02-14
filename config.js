d858f26c5298cf87fd43cab59b5bda57d5f158f6
Prasis Rijal <51752806+Prasis73@users.noreply.github.com>
2023-02-14 15:34:33 +0545

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyCjmiIX8H2ty0bgCeetCuJpx51VlmZgW7g",
-  authDomain: "upwork-df50e.firebaseapp.com",
-  projectId: "upwork-df50e",
-  storageBucket: "upwork-df50e.appspot.com",
-  messagingSenderId: "266788837318",
-  appId: "1:266788837318:web:5685af3905de9ca25b84c7",
-  measurementId: "G-DK2EK7FQF1"
-}; 
